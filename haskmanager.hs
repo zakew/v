8c4712839c6ecec62983e49faab04134068329a8
ZaggyDoo <pouriakaramiprivat@gmail.com>
2022-02-16 22:10:22 +0100

    function templates

---
+++ haskmanager.hs
@@ -0,0 +1,36 @@
+main :: IO ()
+  {- main 
+     RETURNS: ... 
+     SIDE EFFECTS: 
+     EXAMPLES:
+   -}
+
+function :: ... -> ...
+  {- empty function x
+     RETURNS: ...
+   -}
+function = undefined 
+   
+function :: ... -> ...
+  {- empty function x
+     RETURNS: ...
+   -}
+function = undefined 
+   
+function :: ... -> ...
+  {- empty function x
+     RETURNS: ...
+   -}
+function = undefined 
+
+function :: ... -> ...
+  {- empty function x
+     RETURNS: ...
+   -}
+function = undefined 
+
+function :: ... -> ...
+  {- empty function x
+     RETURNS: ...
+   -}
+function = undefined 
