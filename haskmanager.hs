5007c6df74cc145c29be783566a4d7a89f243574
zakew <90333432+zakew@users.noreply.github.com>
2022-02-17 18:47:12 +0100

    Update haskmanager.hs

--- haskmanager.hs
+++ haskmanager.hs
@@ -3,8 +3,12 @@ main :: IO ()
   {- main 
      RETURNS: ... 
      SIDE EFFECTS: 
-     EXAMPLES:
+     EXAMPLES: 
    -}
+main = do
+    putStrLn  "\nWelcome to your HaskMonitor\n\nMenu                           🚩 - important\n1: All tasks                   🟥 - todo     \n2: Important only              ✅ - done     \n3: List manager\n4: Task manager"
+    action <- getLine
+    putStrLn "You chose to go to" 
 
 function :: ... -> ...
   {- empty function x
